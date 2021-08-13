91e262e74ebc2244592e901544cad50e36a807c8
Karl Sigel <heykarl@gmail.com>
2021-08-13 13:53:19 -0700

    made scoreboard

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // import functions and grab DOM elements
+const duckTouchdown = document.getElementById('ducks-touchdown');
+const corvidsaeTouchdown = document.getElementById('corvidsae-touchdown');
+
+const ducksScore = document.getElementById('ducks-score');
+const corvidsaeScore = document.getElementById('corvidsae-score');
+
+
 
 // initialize state
+let crazy = 0;
+let lazy = 0;
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+duckTouchdown.addEventListener('click', () => {
+    crazy = crazy + 7;
+    ducksScore.textContent = crazy;
+});
+corvidsaeTouchdown.addEventListener('click', () => {
+  lazy = lazy + 7;
+  corvidsaeScore.textContent = lazy;
+});
